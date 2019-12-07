9968acd40833240143b4774eb7f3722096246de1
root <root@vultr.guest>
2019-12-07 13:44:55 +0000

    chapter 05

--- chapter_5/src/main.rs
+++ chapter_5/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let rect1 = Rectangle{width: 30, height: 50};
 
     println!("The area of the rectangle is {} square pixels.", area(&rect1));
-    println!("rect1 = {:?}", rect1);
+    println!("rect1 = {:#?}", rect1);
 }
 
 fn area(rect: &Rectangle)->u16{
