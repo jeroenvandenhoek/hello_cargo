67659fe6ebbde6c784429d0c6c950f3e846573a6
root <root@vultr.guest>
2019-12-07 13:21:02 +0000

    chapter 5 printing in debug mode

--- chapter_5/src/main.rs
+++ chapter_5/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u16,
     height: u16,
@@ -7,6 +8,7 @@ fn main() {
     let rect1 = Rectangle{width: 30, height: 50};
 
     println!("The area of the rectangle is {} square pixels.", area(&rect1));
+    println!("rect1 = {:?}", rect1);
 }
 
 fn area(rect: &Rectangle)->u16{
